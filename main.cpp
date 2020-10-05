ed312bfa62788f85a83b1f7fcce3f93a2244e426
pablopascu99 <59370966+pablopascu99@users.noreply.github.com>
2020-10-05 20:07:21 +0200

    Segunda version

--- main.cpp
+++ main.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
 using namespace std;
+struct fecha{
+    int dia;
+    int mes;
+    int ano;
+};
 struct vox{
     char nombreApellido;
     int exp;
+    fecha fecha;
     vox *sgt;
 };
 vox *peek = NULL;
-void push(char nA,int num){
+void pushInt(int num){
     vox *nvox =  new vox;
     nvox->exp=num;
+    nvox->sgt=peek;
     peek=nvox;
 }
-void pushChar(){
+void pushChar(char nA){
     vox *nvox =  new vox;
     nvox->nombreApellido=nA;
+    nvox->sgt=peek;
+    peek=nvox;
+}
+void pushFecha(fecha f){
+    vox *nvox =  new vox;
+    nvox->fecha=f;
+    nvox->sgt=peek;
     peek=nvox;
 }
 void pop(){
     if(peek==NULL)
-        cout<<"Nada que imprimir, Stack vacia \n";
+        cout<<"Nada que sacar, Stack vacia \n";
     else
         peek=peek->sgt;
 }
@@ -30,32 +44,45 @@ void prt(){
         while(aux!=NULL){
             cout<<aux->nombreApellido<<"-";
             cout<<aux->exp<<"-";
+            /*cout<<aux->fecha<<"-";*/
             aux=aux->sgt;
         }
 }
 int main(){
     int op;
     do{
-        cout<<"\n(1) Push    (3) Pop    (4) Print   (5) Salir \n ";
+        cout<<"\n(1)PushChar (2)PushInt (3)PushFecha (4)Pop (5)Print (6) Salir \n ";
         cout<<"\n Elija opcion  ?: ",cin>>op;
         switch (op){
             case 1:
                 char nombreAp;
-                int numExp;
-                cout<<"Ingresa el valor a insertar en el Stack : ??  ",cin>>nombreAp;
-                cout<<"Ingresa el valor a insertar en el Stack : ??  ",cin>>numExp;
-                push(nombreAp,numExp);
+                cout<<"Ingresa el char a insertar en el Stack : ??  ",cin>>nombreAp;
+                pushChar(nombreAp);
                 break;
             case 2:
+                int numExp;
+                cout<<"Ingresa el entero a insertar en el Stack : ??  ",cin>>numExp;
+                pushInt(numExp);
+                break;
+            case 3:
+                int d,m,a;
+                fecha f;
+                cout<<"Ingresa el dia a insertar en fecha : ??  ",cin>>d;
+                cout<<"Ingresa el dia a insertar en fecha : ??  ",cin>>m;
+                cout<<"Ingresa el dia a insertar en fecha : ??  ",cin>>a;
+                f={d,m,a};
+                pushFecha(f);
+                break;
+            case 4:
                 cout<<"Eliminando ..... : ";
                 pop();
                 break;
-            case 3:
+            case 5:
                 cout<<"Imprimiendo..... : ";
                 prt();
                 break;
-            case 4:
+            case 6:
                 cout<<"------Bye--------";
         }
-    }while(op!=4);
+    }while(op!=6);
 }
